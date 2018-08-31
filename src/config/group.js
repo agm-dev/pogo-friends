db4e2e98154fafb26cc8fcb83739fc069015bb6e
Adrian Gonzalo <gonzahh@gmail.com>
2018-08-31 02:12:14 +0200

    added mongoose data models

---
+++ src/config/group.js
@@ -0,0 +1,12 @@
+module.exports = {
+    allowed: [0, 1, 2, 3],
+    allowed_string: ['closed', 'open', 'full', 'validated'],
+    0: 'closed',
+    1: 'open',
+    2: 'full',
+    3: 'validated',
+    closed: 0,
+    open: 1,
+    full: 2,
+    validated: 3
+}
---
+++ src/models/Group.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const group_config = require('../config/group');
+
+const model_name = 'Group';
+const valid_states = group_config.allowed;
+const state_min = Math.min(...valid_states);
+const state_max = Math.max(...valid_states);
+
+const schema = new mongoose.Schema({
+    id: { // hash to share
+        type: String,
+        required: true,
+        index: true, // optional while unique is true
+        unique: true
+    },
+    chat_id: { // chat id from chat service
+        type: Number,
+        required: false, // when group is created there is no chat still
+        index: true,
+        unique: true,
+    },
+    admin: {
+        type: Number,
+        required: true,
+        index: true,
+        unique: false // one user can create multiple groups
+    },
+    size: {
+        type: Number,
+        required: true, // don't create the group till we have all required data
+        min: 2,
+        max: 20
+    },
+    frequency: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 7
+    },
+    members: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    rejected: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+    state: {
+        type: Number,
+        required: true,
+        min: state_min,
+        max: state_max,
+        default: group_config.open
+    } // open, full, validated, closed
+});
+
+module.exports = mongoose.model(model_name, schema);
